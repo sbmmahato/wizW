b284d98b96811966193e13d9a09ce737672a55b6
Subham Mahato <sbmmahato@gmail.com>
2024-07-10 20:36:54 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import db from "@/lib/supabase/db";
+import { Suspense } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   console.log(db);
-  return (
+  return (<Suspense>
     <html lang="en">
       <body className={inter.className}>{children}</body>
-    </html>
+    </html></Suspense>
   );
 }
