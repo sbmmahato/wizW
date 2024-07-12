ab1f70df6b78aa99dd2f639b57325374b4e8131e
Subham Mahato <sbmmahato@gmail.com>
2024-07-12 17:59:15 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
+const config = {
+    reactStrictMode: true,
+    webpack: (webpackConfig, { webpack }) => {
+      webpackConfig.plugins.push(
+        // Remove node: from import specifiers, because Next.js does not yet support node: scheme
+        // https://github.com/vercel/next.js/issues/28774
+        new webpack.NormalModuleReplacementPlugin(
+          /^node:/,
+          (resource) => {
+            resource.request = resource.request.replace(/^node:/, '');
+          },
+        ),
+      );
+  
+      return webpackConfig;
+    },
+  };
+  
+  export default config;
